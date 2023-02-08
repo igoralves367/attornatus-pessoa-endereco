2725c151c7f46c171c1564b088d7b6f3ab66a575
igoralves367 <igor.alves367@gmail.com>
2023-02-08 19:10:13 -0300

    Implementa Infra e DataJPA, teste ok

--- src/main/java/br/com/attornatus/pessoaendereco/endereco/application/service/EnderecoApplicationService.java
+++ src/main/java/br/com/attornatus/pessoaendereco/endereco/application/service/EnderecoApplicationService.java
@@ -2,15 +2,28 @@ package br.com.attornatus.pessoaendereco.endereco.application.service;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import br.com.attornatus.pessoaendereco.endereco.application.api.EnderecoRequest;
 import br.com.attornatus.pessoaendereco.endereco.application.api.EnderecoResponse;
-
+import br.com.attornatus.pessoaendereco.endereco.application.repository.EnderecoRepository;
+import br.com.attornatus.pessoaendereco.endereco.domain.Endereco;
+import br.com.attornatus.pessoaendereco.pessoa.application.service.PessoaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+@Service
+@Log4j2
+@RequiredArgsConstructor
 public class EnderecoApplicationService implements EnderecoService {
+	private final PessoaService pessoaService;
+	private final EnderecoRepository enderecoRepository;
 
 	@Override
 	public EnderecoResponse criaEndereco(UUID idPessoa, EnderecoRequest enderecoRequest) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("[inicia] EnderecoApplicationService - criaEndereco");
+		pessoaService.buscaPessoaAtravesId(idPessoa);
+		Endereco endereco = enderecoRepository.salvaEndereco(new Endereco(idPessoa, enderecoRequest));
+		log.info("[finaliza] EnderecoApplicationService - criaEndereco");
+		return new EnderecoResponse(endereco.getIdEndereco());
 	}
-
 }
