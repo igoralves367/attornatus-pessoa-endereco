9646b900c2784f69d16dac2509323c88152ae6d4
igoralves367 <igor.alves367@gmail.com>
2023-02-08 18:11:18 -0300

    Implementa Requeste e Response

--- src/main/java/br/com/attornatus/pessoaendereco/endereco/application/api/EnderecoRequest.java
+++ src/main/java/br/com/attornatus/pessoaendereco/endereco/application/api/EnderecoRequest.java
@@ -1,4 +1,16 @@
 package br.com.attornatus.pessoaendereco.endereco.application.api;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.Value;
+@Value
 public class EnderecoRequest {
+	@NotBlank
+	private String lagradouro;
+	@NotBlank
+	private String cep;
+	@NotBlank
+	private String numero;
+	@NotBlank
+	private String cidade;
 }
